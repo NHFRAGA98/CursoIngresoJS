a8027f0503c359563ea355627df26bb01498af40
NHFRAGA98 <60699874+NHFRAGA98@users.noreply.github.com>
2020-02-05 11:18:02 -0300

    SUMA, DIVISIÓN
    Practica

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,7 +4,18 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var primerNumero;
+	var segundoNumero; 
+	var resultado;
+	primerNumero= document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero);
+	segundoNumero= document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero);
+
+	 resultado= primerNumero+segundoNumero;
+
+	console.log (resultado); 
+
 }
 
 function restar()
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,21 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+	var primerNumero;
+	var segundoNumero; 
+	var resultado;
+
+	primerNumero=document.getElementById('numeroDividendo').value;
+	primerNumero=parseInt(primerNumero);
+	console.log ("el numero es" + primerNumero); 
+	segundoNumero=document.getElementById('numeroDivisor').value;
+	segundoNumero=parseInt(segundoNumero);
+	console.log ("el numero es" + segundoNumero); 
+	
+
+
+	 resultado= primerNumero%segundoNumero;
+	 console.log(resultado);
+
 	
 }
